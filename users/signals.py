6e1a50d7bd21dc19471250d853bf252abc36ec74
hadi sql <hadi.sqalli@laplateforme.io>
2023-07-03 21:04:23 +0200

    signals edited

--- users/signals.py
+++ users/signals.py
@@ -7,8 +7,13 @@ from .models import UserProfile
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
-        UserProfile.objects.create(user=instance)
+        UserProfile.objects.create(user=instance,
+                                   first_name=instance.first_name,
+                                   last_name=instance.last_name,
+                                   email=instance.email)
+        print('UserProfile created !')
 
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     instance.userprofile.save()
+    print('UserProfile updated !')
