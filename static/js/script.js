f750460f0a77628a91278e66a333322c841d0c8d
hadi sql <hadi.sqalli@laplateforme.io>
2023-07-10 15:13:28 +0200

    toggle menu and styling fixed

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,30 @@
+// Nav Bar Hamburger Menu
+const btnHamburger = document.querySelector('#btnHamburger');
+const menu = document.querySelector('#menu');
+const span1 = document.getElementById('span1');
+const span2 = document.getElementById('span2');
+const span3 = document.getElementById('span3');
 
+btnHamburger.addEventListener('click', () => {
+  console.log('click hamburger');
+
+  if(menu.classList.contains('hidden')){ // Open Hamburger Menu Toggle
+    menu.classList.remove('hidden');
+    span1.classList.add('open-span1');
+    span2.classList.add('open-span2');
+    span3.classList.add('open-span3');
+    console.log('open hamburger');
+  } else {
+    menu.classList.add('hidden');
+    span1.classList.remove('open-span1');
+    span2.classList.remove('open-span2');
+    span3.classList.remove('open-span3');
+    console.log('close hamburger');
+  }
+});
+
+
+// Profile Update Avatar
 if (window.location.pathname === profileUpdateUrl) {
 
   const avatar = document.querySelector('#avatar')
