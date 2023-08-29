039cefe3f36242d08f8e104673cfb74218bd8315
hadi sql <hadi.sqalli@laplateforme.io>
2023-08-29 21:40:40 +0200

    removed undesired gitignore file from cached

--- notes_app/settings/dev.py
+++
@@ -1,9 +0,0 @@
-
-from .base import *
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['127.0.0.1']
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)j5x-4koqn4c)rw+u_fd8vy@1x(1(ldwab5u!gzwdg-l7qw_-g'
